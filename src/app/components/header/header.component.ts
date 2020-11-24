77d49c6e6dbe888e14ca2cb366ce554b651b464b
Maarten Bakker <maartenbakker@gmail.com>
2020-11-24 21:17:50 +0100

    implemented navigation without routing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output()
+  public changePageEmitter = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelect(selection: string): void {
+    this.changePageEmitter.emit(selection);
+    // console.log('emitting recipes');
   }
-
 }
--- src/app/model/recipe.model.ts
+++ src/app/model/recipe.model.ts
@@ -1,7 +1,7 @@
 export class Recipe {
   public name: string;
   public description: string;
-  public imagePath: string = '';
+  public imagePath: string = 'recipes';
 
   constructor(name: string, description: string, imagePath: string) {
     this.name = name;
